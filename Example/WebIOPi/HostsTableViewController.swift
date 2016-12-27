24907e2369eb3901661c947415bf00f273284f0d
Isaac Overacker <ioveracker@cbtnuggets.com>
2016-12-27 09:49:30 -0800

    New view controller to add hosts & launch screen updates.

---
+++ Example/WebIOPi/HostsTableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  HostsTableViewController.swift
+//  WebIOPi
+//
+//  Created by Isaac Overacker on 12/27/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import WebIOPi
+
+class HostsTableViewController: UITableViewController {
+
+    fileprivate var hosts = [WebIOPi]()
+
+    @IBAction func addHostButtonTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(
+            title: "Add Host",
+            message: "Enter the host URL:",
+            preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Add", style: .default) { action in
+            if let textField = alert.textFields?.first {
+                let host = textField.text!
+
+                guard !self.hosts.contains(where: { $0.host == host }) else {
+                    let duplicateAlert = UIAlertController(
+                        title: "Duplicate Host",
+                        message: "Host '\(host)' already exists.",
+                        preferredStyle: .alert)
+                    duplicateAlert.addAction(
+                        UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(duplicateAlert, animated: true, completion: nil)
+                    return
+                }
+
+                self.addHost(host: host)
+            }
+        })
+
+        alert.addTextField() { textField in
+            textField.placeholder = "Host URL"
+        }
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    // MARK: UITableViewDataSource
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hosts.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HostCell")!
+        cell.textLabel?.text = hosts[indexPath.row].host
+        return cell
+    }
+
+}
+
+fileprivate extension HostsTableViewController {
+
+    func addHost(host: String) {
+        hosts.append(WebIOPi(host: host))
+        tableView.reloadData()
+    }
+
+}
